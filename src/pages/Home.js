3d60db4489c3e2b53aa09cc321ea7f068f511e66
Axel Espinosa <axeldavid45@gmail.com>
2020-06-28 10:42:39 -0500

    Home function added with template for character

---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+const Home = () => {
+    return `
+    <div class="Characters">
+        <article class="Character-item">
+           <a href="#/1/">
+            <img src="image" alt="name">
+           </a>
+        </article>
+    </div>
+    `;
+}
+
+export default Home;
