ca0b0c797fbc519f5576a4133d1e28ac293b6039
Axel Espinosa <axeldavid45@gmail.com>
2020-07-04 11:46:42 -0500

    Function getData and rendered of characters in home done

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,17 @@
-const Home = () => {
+import getData from "../utils/getData";
+
+const Home = async () => {
+    const characters = await getData();
     return `
     <div class="Characters">
-        <article class="Character-item">
-           <a href="#/1/">
-            <img src="image" alt="name">
-           </a>
-        </article>
+        ${characters.results.map(character => `
+            <article class="Character-item">
+                <a href="#/${character.id}/">
+                    <img src="${character.image}" alt="${character.name}">
+                    <h3>${character.name}</h3>
+                </a>
+            </article> 
+        `).join('')}
     </div>
     `;
 }
